1354d27cc37c4065c3547aaeeef64a4c44722567
TankMissile <trevans1@yahoo.com>
2015-05-08 16:03:02 -0500

    Added wooden platforms + Added wooden platforms, can be walked on and jumped through.  You can pass through them from above by holding the crouch button. + Added models for wooden platforms (no texture) + Created spider web graphic, not in use * Fixed invisible walls acting like actual walls * Tested playing with xbox controller, seems to work splendidly

---
+++ SBGReborn/Assets/Code/Managers/PlatformDisabler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDisabler : MonoBehaviour {
+	public Collider2D box;
+
+	float timeToDisable = -1;
+
+	void Update () {
+		if(Input.GetButtonDown("Crouch")){
+			timeToDisable = .3f;
+		}
+		else if(Input.GetButtonUp("Crouch")){
+			box.enabled = true;
+			timeToDisable = -1;
+		}
+		
+		
+		if(timeToDisable > 0){
+			timeToDisable -= Time.deltaTime;
+			if(timeToDisable <= 0){
+				box.enabled = false;
+			}
+		}
+	}
+}
